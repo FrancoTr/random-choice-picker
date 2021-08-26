6a166c4cae535b9f1346cdcaa24fab94d7b1abd7
Franco Traverso <franco-t20@outlook.com>
2021-08-26 11:07:42 -0300

    tags get trimmed and cannot be empty strings

--- script.js
+++ script.js
@@ -8,5 +8,8 @@ textArea.addEventListener("keyup", (e) => {
 });
 
 function createTags(input) {
-  const tags = input.split(",");
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
 }
