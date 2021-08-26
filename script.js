681ef57aac1ed01ed75b707613c3e9f4cf5abf39
Franco Traverso <franco-t20@outlook.com>
2021-08-26 11:03:30 -0300

    add createTags function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const tagsEl = document.getElementById("tags");
 const textArea = document.getElementById("textarea");
 
-textArea.focus();
+textArea.focus(); //automatically focus on the textarea
+
+textArea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input.split(",");
+}
